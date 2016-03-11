317490561bdf3d36aa26b8e12f06424168478ae9
John Preston <wcerfgba@riseup.net>
2016-03-11 18:02:21 +0000

    Use category objects, not IDs; wrap category content in div.

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <?php foreach ( get_categories( "orderby=id" ) as $catid) : ?>
-            <div id="category-<?php echo $catid; ?>" class="category">
-                <div id="category-<?php echo $catid; ?>-name-div" class="category-name-div">
-                    <h2 class="category-name"><?php get_cat_name( $catid ); ?></h2>
+        <?php foreach ( get_categories( "orderby=id" ) as $cat) : ?>
+            <div id="category-<?php echo $cat->cat_ID; ?>" class="category">
+                <div id="category-<?php echo $cat->cat_ID; ?>-name-div" class="category-name-div">
+                    <h2 class="category-name"><?php echo $cat->cat_name; ?></h2>
                 </div>
-            <?php $cat_query = new WP_Query( "cat=$catid" );
-            while ( $cat_query->have_posts() ) : $cat_query->the_post(); ?>
-                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <div id="post-<?php the_ID(); ?>-thumbnail" class="post-thumbnail">
-                        <?php the_post_thumbnail(); ?>
+                <div id="category-<?php echo $cat->cat_ID; ?>-content-div" class="category-content-div">
+                <?php $cat_query = new WP_Query( "cat=$cat->cat_ID" );
+                while ( $cat_query->have_posts() ) : $cat_query->the_post(); ?>
+                    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div id="post-<?php the_ID(); ?>-thumbnail" class="post-thumbnail">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                        <div id="post-<?php the_ID(); ?>-title" class="post-title">
+                            <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+                        </div>
                     </div>
-                    <div id="post-<?php the_ID(); ?>-title" class="post-title">
-                        <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-                    </div>
+                <?php endwhile; ?>
                 </div>
-            <?php endwhile; ?>
-        <?php endfor; ?>
+        <?php endforeach; ?>
     </main>
 </div>
 <?php get_footer(); ?>
