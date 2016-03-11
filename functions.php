3bd568c5d0e319592c7103a0f5c4aff746b7f4b8
John Preston <wcerfgba@riseup.net>
2016-03-11 18:54:32 +0000

    Enqueue jQuery.

--- functions.php
+++ functions.php
@@ -3,9 +3,15 @@
 function wardrobe_enqueue_scripts() {
 	wp_enqueue_style( 'core', get_stylesheet_uri(), false ); 
 	
+	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/jquery.js', false );
 	wp_enqueue_script( 'script', get_template_directory_uri() . '/script.js', false );
 }
 
+function wardrobe_widgets_init() {
+    register_sidebar();
+}
+
 add_action( 'wp_enqueue_scripts', 'wardrobe_enqueue_scripts' );
+add_action( 'widgets_init', 'wardrobe_widgets_init' );
 
 ?>
